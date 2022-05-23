e73147d7bc1c3366ceb3a5d99b4ca8892ee7408e
Carlos Bentin <cbentin12@gmail.com>
2022-05-23 15:51:12 -0500

    adding validations

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -47,14 +47,14 @@ def createPlace(city_id):
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    user = storage.get(User, request.get_json()["user_id"])
+    user = storage.get(User, request.get_json().get("user_id"))
     if user is None:
         abort(404)
     dict = request.get_json()
     dict["city_id"] = city_id
     new_instance = Place(**dict)
     new_instance.save()
-    return make_response(jsonify(new_instance.to_dict(), 201))
+    return make_response(jsonify(new_instance.to_dict()), 201)
 
 
 @app_views.route("/places/<string:place_id>", methods=["DELETE"],
@@ -66,7 +66,7 @@ def deletePlace(place_id):
         abort(404)
     place.delete()
     storage.save()
-    return (jsonify({}))
+    return jsonify({})
 
 
 @app_views.route("/places/<string:place_id>", methods=["PUT"],
@@ -79,7 +79,7 @@ def updatePlace(place_id):
     if not request.get_json():
         return make_response(jsonify({"error": "Not a Json"}), 400)
     for key, val in request.get_json().items():
-        if key not in ['id', 'created_at', 'updated_at']:
+        if key not in ["id", "user_id", "city_id", "created_at", "updated_at"]:
             setattr(place, key, val)
-    place.save()
-    return jsonify(place.to_dict())
+    storage.save()
+    return make_response(jsonify(place.to_dict()), 200)
